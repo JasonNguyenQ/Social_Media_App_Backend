03f2a02ceb83cb8a6ef88574ca61c12fb5aa0bb1
Master-AMW <lgnjasonn@gmail.com>
2025-03-19 18:37:59 -0700

    Added API route to update profile

--- api/Users.js
+++ api/Users.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const multer = require("multer")
 const asyncHandler = require('express-async-handler')
+const { validateBufferMIMEType } = require("validate-image-type")
 const Authenticate = require("./Auth")
 const bcrypt = require('bcryptjs')
 const connection = require("../database/database")
 
 const app = express.Router()
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 app.get('/api/users/', asyncHandler(async (req,res)=>{
     const { id } = req.query
@@ -35,7 +39,38 @@ app.post('/api/users/register', asyncHandler(async (req,res)=>{
     res.status(200).send("SUCCESSFULLY CREATED USER")
 }));
 
+app.patch('/api/users', Authenticate, 
+    upload.fields([
+    { name: 'profilePicture', maxCount: 1 }, 
+    { name: 'backgroundImage', maxCount: 1 }]),
+    asyncHandler(async (req,res)=>{
+    const id = req.id;
+    const {profilePicture, backgroundImage} = req.files
+    const allowedTypes = ['image/png', 'image/jpg', 'img/jpeg']
+
+    let query = 'UPDATE `users` SET ';
+    const params = []
+    if(req.body.description){
+        query += "description = ?, "
+        params.push(req.body.description)
+    }
+    if(profilePicture && await validateBufferMIMEType(profilePicture[0].buffer, {allowMimeTypes: allowedTypes})){
+        query += "profilePicture = ?, "
+        params.push(profilePicture[0].buffer.toString('base64'))
+    }
+    if(backgroundImage && await validateBufferMIMEType(backgroundImage[0].buffer, {allowMimeTypes: allowedTypes})){
+        query += "backgroundImage = ?, "
+        params.push(backgroundImage[0].buffer.toString('base64'))
+    }
+    query = query.slice(0,-2) + " WHERE id = ?"
+    params.push(id)
+    console.log(profilePicture)
+    await connection.execute(query, params)
+    res.status(200).send("SUCCESSFULLY UPDATED USER")
+}));
+
 app.use((err,req,res,next)=>{
+    console.log(err)
     res.status(500).send(err)
 })
 
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const asyncHandler = require('express-async-handler')
 const jwt = require("jsonwebtoken")
 const http = require('http')
 const { Server } = require('socket.io')
+const cookieParser = require('cookie-parser')
 const cors = require('cors');
 const bcrypt = require('bcryptjs')
 const connection = require('./database/database')
@@ -10,14 +11,17 @@ const connection = require('./database/database')
 const app = express()
 
 app.use(express.json())
+app.use(cookieParser())
 
 const corsOptions = {
-    origin: `http://localhost:${process.env.SERVER_PORT}`,
-    methods: 'GET,POST,PUT,DELETE,PATCH',
-    allowedHeaders: 'Content-Type,Authorization',
-    credentials: true
-} //DO LATER
-app.use(cors())
+    origin: [`http://localhost:3000`,`http://localhost:5173`],
+    methods: ['GET','POST','PUT','DELETE','PATCH'],
+    allowedHeaders: ['Content-Type','Authorization'],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
 
 const friendsRouter = require('./api/Friends')
 const usersRouter = require('./api/Users')
@@ -74,25 +78,30 @@ app.get('/', (req,res)=>{
 app.post('/auth/login', asyncHandler(async (req,res)=>{
     const { username, password } = req.body
     
-    connection.execute(
+    const [result] = await connection.execute(
         'SELECT id, password FROM `users` WHERE username = ?',
         [username]
     )
-    .then(async ([result])=>{
-        const hashedPassword = result[0].password
-        const success = await bcrypt.compare(password,hashedPassword)
-        if(success){
-            const user = { sub: result[0].id, preferred_username: username }
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-            res.json({ access_token : token })
-        }
-        else{
-            res.status(401).send("UNAUTHORIZED")
-        }
-    })
-    .catch((err)=>{
-        res.status(401).send(err)
-    })
+    
+    const hashedPassword = result[0].password
+    const success = await bcrypt.compare(password,hashedPassword)
+
+    if(success){
+        const user = { sub: result[0].id, preferred_username: username }
+        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+        const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+        res.cookie("refresh_token", refreshToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none',
+            maxAge: 1000*60*60*24*60,
+            path: '/'
+        })
+        res.status(200).json({ access_token : accessToken })
+    }
+    else{
+        res.status(401).send("UNAUTHORIZED")
+    }
 }))
 
 app.get('/auth/authenticate', (req,res)=>{
