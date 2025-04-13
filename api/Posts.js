7b0e13a2355593e8dc262da923046c2969efd675
Master-AMW <lgnjasonn@gmail.com>
2025-04-13 16:52:28 -0700

    Fixed image uploading in posts API

--- api/Posts.js
+++ api/Posts.js
@@ -3,6 +3,7 @@ const asyncHandler = require('express-async-handler')
 const Authenticate = require("./Auth")
 const { validateBufferMIMEType } = require("validate-image-type")
 const multer = require("multer")
+const sharp = require("sharp")
 const {DBConnection} = require("../database/database")
 
 const app = express.Router()
@@ -13,13 +14,13 @@ const upload = multer({
     limits: { fileSize: 1024*1024 }
 });
 
-app.post('/api/posts', Authenticate, upload.single('image'), asyncHandler(async (req, res)=>{
+app.post('/api/posts', Authenticate, upload.single('image'), (async (req, res)=>{
     const { title, caption } = req.body
-    const { image } = req.files
+    const image = req.file
     const allowedTypes = ['image/png', 'image/jpg', 'img/jpeg']
 
-    if(image && await validateBufferMIMEType(image[0].buffer, {allowMimeTypes: allowedTypes})){
-        const compressedImage = await sharp(profilePicture[0].buffer)
+    if(image && await validateBufferMIMEType(image.buffer, {allowMimeTypes: allowedTypes})){
+        const compressedImage = await sharp(image.buffer)
             .webp({quality: 80})
             .toBuffer()
 
