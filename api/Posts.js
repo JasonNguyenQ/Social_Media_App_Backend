81cdb9da2c7c1f73909a852780398b048d51922c
Master-AMW <lgnjasonn@gmail.com>
2025-04-23 10:13:23 -0700

    Added API for posting and getting comments

--- api/Posts.js
+++ api/Posts.js
@@ -48,6 +48,31 @@ app.post('/api/posts', Authenticate, upload.single('image'), asyncHandler(async 
     res.status(200).send("POST CREATED")
 }));
 
+app.get('/api/posts/comments/:id', asyncHandler(async (req, res)=>{
+    const postId = req.params.id
+
+    const [rows] = await DBConnection.execute(`
+        SELECT id, profilePicture, username AS 'from', comment, createdAt
+        FROM comments JOIN users ON users.id = comments.userId
+        WHERE postId = ?`,
+        [postId]
+    )
+
+    res.status(200).send(rows)
+}));
+
+app.post('/api/posts/comments', Authenticate, asyncHandler(async (req, res)=>{
+    const { postId, comment } = req.body
+
+    await DBConnection.execute(`
+        INSERT INTO comments 
+        (postId, userId, comment, createdAt) 
+        VALUES (?,?,?,?)`,
+        [postId, req.id, comment, new Date()]
+    )
+    res.status(200).send("COMMENT CREATED")
+}));
+
 app.use((err,req,res,next)=>{
     res.status(500).send(err)
 })
